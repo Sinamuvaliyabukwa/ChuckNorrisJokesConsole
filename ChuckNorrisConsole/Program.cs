caafff2a57041d87971041c55c558f725f88fc12
Sinamuvaliyabukwa <lindeniace08@gmail.com>
2019-10-02 15:11:55 +0200

    Initial Chuck Norris Commit

---
+++ ChuckNorrisConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+
+namespace ChuckNorrisConsole
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            string randomJoke = await jokeGenerator.GetRandomJoke();
+           Console.WriteLine(randomJoke);
+
+
+
+            string[] categories = await jokeGenerator.GetCategories();
+            for (int index = categories.Length - 1; index >= 0; index--)
+            {
+                Console.WriteLine(categories[index]);
+            }
+        }
+
+        
+    }
+}
